dc972485e61e55aeabc89ef108e744d78738e127
Justin Bull <bull.justin@gmail.com>
2017-11-26 14:39:49 -0800

    Use relative paths on server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('/dist'));
+app.use(express.static('./dist'));
 
 app.get('/', (req, res) => {
-  res.sendFile('/dist/index.html');
+  res.sendFile('./dist/index.html');
 });
 
 app.listen(80);
