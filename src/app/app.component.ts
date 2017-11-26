810da03a4140cc8b468c03243b744447bdf4bbbe
Justin Bull <bull.justin@gmail.com>
2017-11-26 05:31:04 -0800

    Create loop for social media links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +6,31 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['app.component.styl']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  constructor() {
-  }
+  socials = [
+    {
+      icon: '/assets/icons/linkedin.svg',
+      url: 'https://www.linkedin.com/in/jwbull'
+    },
+    {
+      icon: '/assets/icons/instagram.svg',
+      url: 'https://www.instagram.com/bull.justin'
+    },
+    {
+      icon: '/assets/icons/stackoverflow.svg',
+      url: 'https://stackoverflow.com/users/229787/justin-bull'
+    },
+    {
+      icon: '/assets/icons/github.svg',
+      url: 'https://github.com/jabes'
+    },
+    {
+      icon: '/assets/icons/envelope.svg',
+      url: 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=bull.justin@gmail.com&su=Let%27s%20talk'
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
 }
