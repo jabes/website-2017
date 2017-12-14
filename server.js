3880a31bc43bbf6bf909d7197071bd69d7e33615
Justin Bull <bull.justin@gmail.com>
2017-12-13 22:39:21 -0800

    Update dist path to public

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('./dist/index.html');
+  res.sendFile('./public/index.html');
 });
 
 app.listen(80);
