9d811db9ee6fdcc525d38d293a87034a335df1b4
Justin Bull <bull.justin@gmail.com>
2019-10-29 21:11:49 -0700

    Add resume icon/button to page header

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -10,23 +10,33 @@ export class AppComponent implements OnInit {
   socials = [
     {
       icon: '/assets/icons/linkedin.svg',
-      url: 'https://www.linkedin.com/in/jwbull'
+      url: 'https://www.linkedin.com/in/jwbull',
+      label: 'See My LinkedIn Profile',
     },
     {
       icon: '/assets/icons/instagram.svg',
-      url: 'https://www.instagram.com/bull.justin'
+      url: 'https://www.instagram.com/bull.justin',
+      label: 'See My Photography',
     },
     {
       icon: '/assets/icons/stackoverflow.svg',
-      url: 'https://stackoverflow.com/users/229787/justin-bull'
+      url: 'https://stackoverflow.com/users/229787/justin-bull',
+      label: 'See My Contributions On Stack Overflow',
     },
     {
       icon: '/assets/icons/github.svg',
-      url: 'https://github.com/jabes'
+      url: 'https://github.com/jabes',
+      label: 'See My Work On Github',
     },
     {
-      icon: '/assets/icons/envelope.svg',
-      url: 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=bull.justin@gmail.com&su=Let%27s%20talk'
+      icon: '/assets/icons/contact.svg',
+      url: 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=bull.justin@gmail.com&su=Let%27s%20talk',
+      label: 'Send Me A Message',
+    },
+    {
+      icon: '/assets/icons/resume.svg',
+      url: '/assets/docs/resume.pdf',
+      label: 'See My Latest Resume',
     },
   ];
 
